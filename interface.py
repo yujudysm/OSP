5e4eacc2752f3de6e09b3859bf6c64e94c9f6c00
yujudysm <jyoung11@sookmyung.ac.kr>
2026-04-18 16:43:05 +0900

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,35 @@
+# Inference for ONNX model
+import cv2
+import time
+import requests
+import random
+import numpy as np
+import onnxruntime as ort
+from PIL import Image
+from pathlib import Path
+from collections import OrderedDict, namedtuple
+
+# 설정
+cuda = True
+w = "yolov7-tiny.onnx"
+img_path = 'horses.jpg' # 이미지 경로
+
+# 실행 환경 설정 (CUDA 가능 여부에 따라 Provider 선택)
+providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
+session = ort.InferenceSession(w, providers=providers)
+
+def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = im.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better val mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    
+    # 이 이후의 패딩 및 리사이즈 로직이 이어집니다...
